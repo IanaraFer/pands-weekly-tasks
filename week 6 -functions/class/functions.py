1cdf3ed9258cabfa6b2ae72430c3f66f3d7259d5
Ianara Fernandes <158167439+IanaraFer@users.noreply.github.com>
2024-03-06 14:26:33 +0000

    same

---
+++ week 6 -functions/class/functions.py
@@ -0,0 +1,8 @@
+# defination of functions
+# class exemple
+
+# def is defined ex: def nameofffunction(argument1, argumento2)
+                        #statemnts
+                        #return value
+                        
+print(1, 2, 3)
