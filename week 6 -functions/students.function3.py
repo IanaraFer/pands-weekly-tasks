b1a44eda1c3bbd54f269c69400c2a7e40319f20f
Ianara Fernandes <ianarafer@gmail.com>
2024-03-12 22:36:59 +0000

    file in the past

---
+++ week 6 -functions/students.function3.py
@@ -0,0 +1,12 @@
+students= []
+def readModules():
+ return []
+def doAdd(students):
+ currentStudent = {}
+ currentStudent["Andrew"]=input("Marie:")
+ currentStudent["modules"]= readModules()
+ students.append(currentStudent)
+#test
+doAdd(students)
+doAdd(students)
+print (students)
